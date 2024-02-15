dcda7a8e0e4ae1346f8d8e3af46e56cbca2bf3c6
VLDeMenezes <estudiodemenezes@gmail.com>
2024-02-15 09:58:41 -0300

    html y css

---
+++ scripts/index.js
@@ -0,0 +1,40 @@
+function agregarTec() {
+  let nombre = document.getElementById("nombreTec").value;
+  let imagen = document.getElementById("imagenTec").value;
+
+  document.getElementById("sectionTec").insertAdjacentHTML(
+    "beforeend",
+    `<article class="card">
+      <h3>${nombre}</h3>
+      <img src="${imagen}" alt="Logo de ${nombre}" />
+    </article>`
+  );
+  document.getElementById("nombreTec").value = "";
+  document.getElementById("imagenTec").value = "";
+}
+
+function agregarAct() {
+  let nombre = document.getElementById("nombreAct").value;
+  let descripcion = document.getElementById("descripcionAct").value;
+  let url = document.getElementById("urlImgAct").value;
+  let text = document.getElementById("textAct");
+  let padre = document.getElementById("containerAct");
+
+  if (text) {
+    padre = text.parentNode;
+    padre.removeChild(text);
+  }
+  if (padre.className === "section") padre.className += " container";
+
+  document.getElementById("containerAct").insertAdjacentHTML(
+    "beforeend",
+    `<article class="card imgAct">
+      <h3>${nombre}</h3>
+      <p>${descripcion}</p>
+      <img src="${url}" alt="Imagen de ${nombre}" />
+    </article>`
+  );
+  document.getElementById("nombreAct").value = "";
+  document.getElementById("descripcionAct").value = "";
+  document.getElementById("urlImgAct").value = "";
+}
